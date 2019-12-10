fe15bc36e4ffb13c1937547211e0f56d195f9be7
Jiji (Jihyun Yeonan-Kim) <yeonankim@phototrophique.com>
2019-12-09 17:41:46 -0800

    The getSensitivity_macro is generalilzed to extract the stimulus info from the file name for all the avilable conditions and sort out accordingly instead of having the user specify the conditions

--- getSensitivity_macro.m
+++ getSensitivity_macro.m
@@ -3,56 +3,50 @@ function getSensitivity_macro(parentpath)
 rng; 
 
 % Get all folder names in the 'Results' directory
-d = dir(parentpath)
+d = dir(parentpath);
 isub = [d(:).isdir]; %# returns logical vector
 subFolders = {d(isub).name}'; 
 subFolders(ismember(subFolders,{'.','..'})) = []; 
 
-pathList = fullfile(parentpath, subFolders)
+pathList = fullfile(parentpath, subFolders);
 
 for sub = 1:length(pathList)
     
-    curr_path = pathList{sub}
+    curr_path = pathList{sub};
 
     files_coneresp = dir(fullfile(curr_path, 'ConeExcitationInstances', '*.mat'));
 %     files_coneresp = dir(fullfile(curr_path, 'NoisyConeExcitationInstances', '*.mat'));
 
     for f = 1:length(files_coneresp)
+        exp_list(f) = str2double(cell2mat(extractBetween(files_coneresp(f).name, 'coneExcitation_noiseOff_exp', '_SF'))); 
         sf_list(f) = str2double(cell2mat(extractBetween(files_coneresp(f).name, 'SF_', '_contr'))); 
         contrast_list(f) = str2double(cell2mat(extractBetween(files_coneresp(f).name, '_contr_', '.mat'))); 
     end
-    SFs = unique(sf_list);
-    nSF = length(SFs); 
-    contrasts = unique(contrast_list); 
-    nContrast = length(contrasts); 
+    EXPs = unique(exp_list);           nEXP = length(EXPs);
+    SFs = unique(sf_list);             nSF = length(SFs); 
+    contrasts = unique(contrast_list); nContrast = length(contrasts); 
     
-    result_svm = nan(nSF, nContrast); 
+    result_svm = nan(nSF, nContrast, nEXP); 
     for f = 1:length(files_coneresp)
         
         curr_filename = fullfile(files_coneresp(f).folder, files_coneresp(f).name); 
         load(curr_filename, 'SVMpercentCorrect'); 
         
-        val_sf = cell2mat(extractBetween(files_coneresp(f).name, 'SF_', '_contr'));
+        val_exp = str2double(cell2mat(extractBetween(files_coneresp(f).name, 'coneExcitation_noiseOff_exp', '_SF')));
+%         val_exp = str2double(cell2mat(extractBetween(files_coneresp(f).name, 'coneExcitation_exp', '_SF')));
+        val_sf = str2double(cell2mat(extractBetween(files_coneresp(f).name, 'SF_', '_contr'))); 
         val_contrast = str2double(cell2mat(extractBetween(files_coneresp(f).name, '_contr_', '.mat'))); 
         
-        result_svm(SFs==val_sf, contrasts==val_contrast) = SVMpercentCorrect; 
-%         result_svm(f) = SVMpercentCorrect; % memory of SVM
-
-
-%         % Computing the sesntivitiy
-%         if mod(f, nContrast) == 0
-%             val_exp = cell2mat(extractBetween(files_coneresp(f).name, 'coneExcitation_noiseOff_', '_SF'));
-% %             val_exp = cell2mat(extractBetween(files_coneresp(f).name, 'coneExcitation_', '_SF'));
-%             
-%             val_sensitivity = fitPsychometricFn(contrasts, result_svm(f-(nContrast-1):f));
-%             
-%             fprintf('%s SF%s: %f \n', val_exp, val_sf, val_sensitivity)
-%         end
-        
+        result_svm(SFs==val_sf, contrasts==val_contrast, EXPs==val_exp) = SVMpercentCorrect; 
     end
-    result_svm
+    
+    for i = 1:nEXP
+        for j = 1:nSF
+            val_sensitivity = fitPsychometricFn(contrasts(~isnan(result_svm(j,:,i))), result_svm(j,:,i));
+            fprintf('EXP%d SF%f: %f \n', EXPs(i), SFs(j), val_sensitivity); 
+        end
+    end
+
 end
-
-
 
 end 
